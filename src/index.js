7b37f90777e7154d9fa1a4fb0422f0e7fd162b0b
Svetlana Tur <svetaltur@gmail.com>
2018-02-26 14:52:57 +0200

    almost solved.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var s=[], v=[];
+  var i=2;
+  while (i<=base){
+    while (base>1) {
+      if (base%i==0){
+        s.push(i);
+        base=base/i;
+      }else {i++;}
+    }
+  }
+console.log(s);
+
+
+for (var j=0; j<s.length; j++){
+  var zero=0, k=1;
+  while (number/Math.pow(s[j],k)>=1){
+    zero=zero+(number-number%Math.pow(s[j],k))/Math.pow(s[j],k);
+    k++;
+  }
+  v.push(zero);
 }
+console.log(v);
+
+function compareNumeric(a, b) {
+  if (a > b) return 1;
+  if (a < b) return -1;
+}
+
+
+
+v.sort(compareNumeric);
+
+var zeros=v[0];
+return zeros;
+
+
+}
+
