7a6111abf44d890221d3309aae6044d17b4f8eff
svetlana tur <svetaltur@gmail.com>
2018-02-27 12:42:56 +0300

    solved.

--- src/index.js
+++ src/index.js
@@ -1,39 +1,50 @@
 module.exports = function getZerosCount(number, base) {
   var s=[], v=[];
   var i=2;
+
+var kl=[];
   while (i<=base){
-    while (base>1) {
-      if (base%i==0){
-        s.push(i);
+    var kol=0;
+    if (base%i==0){
+      s.push(i);
+      while (base%i==0){
         base=base/i;
-      }else {i++;}
-    }
+        kol=kol+1;
+      }
+      kl.push(kol);
+    } 
+    i++;
+    
   }
+  
 console.log(s);
-
-
-for (var j=0; j<s.length; j++){
-  var zero=0, k=1;
-  while (number/Math.pow(s[j],k)>=1){
-    zero=zero+(number-number%Math.pow(s[j],k))/Math.pow(s[j],k);
-    k++;
+console.log(kl);
+  for (var j=0; j<s.length; j++){
+    var zero=0, k=1;
+    while (number/Math.pow(s[j],k)>=1){
+      zero=zero+Math.round((number-number%Math.pow(s[j],k))/Math.pow(s[j],k));
+      k++;
+    }
+    v.push(zero);
   }
-  v.push(zero);
+  console.log(v);
+
+  var l=[];
+  for (i=0; i<v.length; i++){
+    l.push(Math.floor(v[i]/kl[i]));
+  }
+
+  function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+  }
+  
+  
+  
+  l.sort(compareNumeric);
+
+ 
+
+  var zeros=l[0];
+  return zeros;
 }
-console.log(v);
-
-function compareNumeric(a, b) {
-  if (a > b) return 1;
-  if (a < b) return -1;
-}
-
-
-
-v.sort(compareNumeric);
-
-var zeros=v[0];
-return zeros;
-
-
-}
-
